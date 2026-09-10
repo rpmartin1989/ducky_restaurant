bbcf4acc6b48b518caed93e91634829fb3a99a71
rpmartin1989 <rpmartin1989@gmail.com>
2026-09-09 22:19:21 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,21 @@
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+
+  link.addEventListener("click", event => {
+
+    const id = link.getAttribute("href");
+    const target = document.querySelector(id);
+
+    if (!target) return;
+
+    event.preventDefault();
+
+    target.scrollIntoView({
+      behavior: "smooth",
+      block: "start"
+    });
+
+    history.replaceState(null, "", id);
+
+  });
+
+});
